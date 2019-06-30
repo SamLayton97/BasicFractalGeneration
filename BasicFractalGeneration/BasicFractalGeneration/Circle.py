1cdd70d3707f3564d9173e7e0fbdb006c6d81a82
Sam Layton <slayton@uccs.edu>
2019-06-30 14:26:08 -0600

    Implemented 'Static Shape' base class for all static shapes to inherit from

--- BasicFractalGeneration/BasicFractalGeneration/Circle.py
+++ BasicFractalGeneration/BasicFractalGeneration/Circle.py
@@ -1,29 +1,22 @@
 import pygame
 import math
-import Constants
+import StaticShape
 
-from Vector import *
 from pygame.locals import *
+from StaticShape import *
 
 # A basic circle
-class Circle:
+class Circle(StaticShape):
 
 	# public variables
-	position = Vector(0, 0)			# centre of circle
 	radius = 0
-	width = 1						# width of circle's edge
-	color = Constants.COLOR_WHITE
-	surface = None					# surface to draw circle on
 
-	# constructor
+	# circle constructor
 	def __init__(self, surface, position, radius, width, color):
-		
+		super().__init__(surface, position, color, width)
+
 		# set circle's attributes
-		self.surface = surface
-		self.position = position
 		self.radius = math.fabs(radius)
-		self.width = math.fabs(width)
-		self.color = color
 
 	# draws circle onto the screen
 	def draw(self, surface):
---
+++ BasicFractalGeneration/BasicFractalGeneration/StaticShape.py
@@ -0,0 +1,28 @@
+import pygame
+import math
+import Constants
+
+from pygame.locals import *
+from Vector import *
+
+# Base class which all static shapes inherit from.
+class StaticShape:
+
+	# shape parameters
+	surface = None					# surface to draw shape on
+	position = Vector(0, 0)			# centre position on shape
+	color = Constants.COLOR_WHITE	# color of shape
+	width = 1						# width of shape's edge, 0 denotes shape is 'filled in'
+
+	# shape constructor
+	def __init__(self, surface, position, color, width):
+		# set base parameters used by all static shapes
+		self.surface = surface
+		self.position = position
+		self.color = color
+		self.width = math.fabs(width)
+
+	# Draws shape onto specified surface.
+	# All child classes must override this method.
+	def draw(self, surface):
+		pass
